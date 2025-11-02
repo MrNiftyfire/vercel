955119f888aeda49a84cbf4167aaf73a564d93f0
Mr Niftyfire <127270926+MrNiftyfire@users.noreply.github.com>
2025-11-03 07:44:21 +0800

    Update subs.js

--- api/subs.js
+++ api/subs.js
@@ -2,7 +2,8 @@
 export default async function handler(req, res) {
   try {
     const response = await fetch(
-      `https://www.googleapis.com/youtube/v3/channels?part=statistics&id=UC0KdIoPfAh_aKEOcBlexMMw&key=${process.env.YT_API_KEY}`
+      `https://www.googleapis.com/youtube/v3/channels?part=statistics&id=UC0KdIoPfAh_aKEOcBlexMMw&key=${process.env.YT_API_KEY
+}`
     );
 
     const data = await response.json();
